e143757d12541300ce3f8b47984fa86bb5120d46
Conor Reedy <conorreedydev@gmail.com>
2025-03-08 14:12:49 -0500

    First step to webserver impl. Greet endpoint and tokio explanation. Macros ftw

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # zero2prod
 Zero to production in Rust
+
+I'm going by the book Zero to Production in Rust and trying to get my Rust chops up. Here's a list of good commands to know when developing this project.
+
+## Cargo
+
+cargo watch -x check # run cargo every time a change is made
+cargo watch -x check -x test -x run # an example of command chaining to build, test, and run the application
+
+
+## Linting 
+cargo clippy
+cargo clippy -- -D warnings #fail if there's warnings
+
+### when getting a bunk suggestion ignore warnings with #[allow(clippy::lint_name)]
+### or in a file outside of code 
+### clippy.toml -> #![allow(clippy::lint_name)]
+
+## Code coverage
+cargo tarpaulin --ignore-tests
+
+## Security 
+cargo-audit # search the Rust Secure Code working group Advisory Database for vulnerable packages
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+// #[get("/")]
+// async fn hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hello world!")
+// }
+
+// #[post("/echo")]
+// async fn echo(req_body: String) -> impl Responder {
+//     HttpResponse::Ok().body(req_body)
+// }
+
+// async fn manual_hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hey there!")
+// }
+
+async fn greet(req: HttpRequest) -> impl Responder {
+
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+
+    HttpServer::new( || {
+        App::new()
+            .route("/",       web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")
+    .expect("REASON")
+    .run()
+    .await
+
 }
